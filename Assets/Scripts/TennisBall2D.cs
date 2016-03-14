20d2d66f2b0fdbf6fcb3401c8f72cf47219275e4
Ben Scott <ias.jazz@gmail.com>
2016-03-13 20:32:02 -0400

    baddie throws tennis balls, mallowdog dies

---
+++ Assets/Scripts/TennisBall2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TennisBall2D : MonoBehaviour {
+
+	public GameObject explosion;
+
+	public float Damage { get { return 20f; } }
+
+
+	void Start() {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		if (!c.rigidbody || c.rigidbody.tag!="Player") return;
+		if (!c.rigidbody.GetComponent<PlayerMovement>()) return;
+		c.rigidbody.GetComponent<PlayerMovement>().Apply(Damage);
+		Object.Instantiate(explosion,transform.position,Quaternion.identity);
+		Destroy(gameObject);
+	}
+}

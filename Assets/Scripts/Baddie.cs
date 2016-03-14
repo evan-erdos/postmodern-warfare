587f5be03d73e365e436b423a273c7b21e4c1f20
Ben Scott <ias.jazz@gmail.com>
2016-03-13 20:32:02 -0400

    baddie throws tennis balls, mallowdog does

--- Assets/Scripts/Baddie.cs
+++ Assets/Scripts/Baddie.cs
@@ -7,52 +7,86 @@ using System.Collections;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Baddie : MonoBehaviour, IDamageable {
 
-	Rigidbody2D rb2d;
-	BoxCollider2D bc2d;
+	bool wait;
+	bool immune;
+	public float range = 8f;
+	public float delay = 2f;
+	public float force = 2f;
 
-	public bool isAlive;
+	LayerMask layerMask;
+
+	Collider2D[] results = new Collider2D[10];
 
 	public GameObject deadReplacement;
+	public GameObject projectile;
 	public GameObject explosion;
-
 	public UnityEvent m_killEvent;
 
 	public void Kill() {
-		isAlive = !isAlive;
-		if (explosion) {
+		if (immune) return;
+		if (explosion)
 			Object.Instantiate(explosion,transform.position,Quaternion.identity);
-			explosion = null;
-		}
-
 		GameObject temp;
 		if (deadReplacement) {
-			temp = Object.Instantiate(deadReplacement,transform.position,Quaternion.identity) as GameObject;
+			temp = Object.Instantiate(
+				deadReplacement,
+				transform.position,
+				Quaternion.identity) as GameObject;
+			var inner = temp.GetComponent<Baddie>();
+			if (inner) inner.Immune(2f);
 			foreach (var rb in temp.GetComponentsInChildren<Rigidbody2D>())
 				rb.AddExplosionForce(4000f, transform.position,400f,200);
-		}
-		Destroy(gameObject);
+		} Destroy(gameObject);
 	}
 
-	/* This kills the crab */
-	public void Die() {
-		rb2d.constraints &= ~(RigidbodyConstraints2D.FreezeAll);
-		bc2d.isTrigger = true;
+	public void Immune(float time) {
+		StartCoroutine(Immunity(time)); }
+
+	public IEnumerator Immunity(float time) {
+		if (immune) yield break;
+		immune = true;
+		yield return new WaitForSeconds(time);
+		immune = false;
 	}
 
 	public void Apply(float damage) {
-		if (damage > 0) {
-			m_killEvent.Invoke();
-			Die();
-		}
-	}
+		if (damage>0) m_killEvent.Invoke(); }
+
+	void Awake() { layerMask = ~LayerMask.NameToLayer("Player"); }
+
 
 	void Start() {
-		rb2d = gameObject.GetComponent<Rigidbody2D>();
-		bc2d = gameObject.GetComponent<BoxCollider2D>();
-
 		if (m_killEvent==null)
 			m_killEvent = new UnityEvent();
 		m_killEvent.AddListener(Kill);
+	}
+
+	IEnumerator Throwing(GameObject target) {
+		if (wait) yield break;
+		wait = true;
+		if (projectile && target) {
+			var instance = Object.Instantiate(
+				projectile,
+				transform.position,
+				Quaternion.identity) as GameObject;
+			var trajectory = new Vector2(
+				target.transform.position.x-transform.position.x,
+				target.transform.position.y-transform.position.y);
+			instance.GetComponent<Rigidbody2D>().AddForce(
+				trajectory*force,ForceMode2D.Impulse);
+		}
+		yield return new WaitForSeconds(delay);
+		wait = false;
+	}
+
+
+	void FixedUpdate() {
+		Physics2D.OverlapCircleNonAlloc(
+			transform.position, range, results, layerMask);
+		foreach (var hit in results)
+			if (hit && hit.attachedRigidbody && hit.attachedRigidbody.tag=="Player")
+				StartCoroutine(Throwing(
+					hit.attachedRigidbody.GetComponent<PlayerMovement>().gameObject));
 	}
 }
 
---
+++ Assets/Scripts/BounceCollider2D.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceCollider2D : MonoBehaviour {
+
+    public float force = 20f;
+
+    void OnTriggerStay2D(Collider2D c) {
+        if (!c.attachedRigidbody) return;
+        var normal = new Vector2(
+        	transform.up.x,
+        	transform.up.y);
+        c.attachedRigidbody.AddForce(normal*force,ForceMode2D.Impulse);
+        //foreach(ContactPoint2D hit in c.contacts)
+        //    c.attachedRigidbody.AddForce(hit.normal*force,ForceMode2D.Impulse);
+    }
+}

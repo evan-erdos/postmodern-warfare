c331947e66437e74157f74cc3cb6ab5ba336661a
Ben Scott <ias.jazz@gmail.com>
2016-03-14 11:39:45 -0400

    basic menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject menu;
+
+	void Awake() {
+		if (!menu)
+			throw new System.Exception("no menu");
+	}
+
+	void Start() {
+		menu.SetActive(false);
+	}
+
+	public void LoadLevel(string scene) {
+		SceneManager.LoadScene(scene);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+
+	void Update() {
+		if (Input.GetButtonDown("Cancel"))
+			menu.SetActive(!menu.activeInHierarchy);
+	}
+
+
+}

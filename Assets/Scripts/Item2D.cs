0569e4a1ab8bf7d82c1b489fcaf416bdbb4240b9
Ben Scott <ias.jazz@gmail.com>
2016-03-21 16:14:37 -0400

    guard fix

--- Assets/Scripts/Item2D.cs
+++ Assets/Scripts/Item2D.cs
@@ -1,25 +1,44 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
-public class Item2D : MonoBehaviour {
+class Item2D : MonoBehaviour {
 
-	public UnityEvent m_getEvent;
+	public UnityEvent m_TakeEvent;
+	public UnityEvent m_DropEvent;
 
-	void Start() {
-		if (m_getEvent==null)
-			m_getEvent = new UnityEvent();
-		m_getEvent.AddListener(Get);
+	void Awake() {
+		if (m_DropEvent==null)
+			m_DropEvent = new UnityEvent();
+		m_DropEvent.AddListener(OnDrop);
+		if (m_TakeEvent==null)
+			m_TakeEvent = new UnityEvent();
+		m_TakeEvent.AddListener(OnTake);
 	}
-
 
 	void OnCollisionEnter2D(Collision2D c) {
-		if (c.rigidbody && c.rigidbody.tag=="Player")
-			m_getEvent.Invoke();
+		if (!c.rigidbody) return;
+		var other = c.rigidbody.GetComponent<PlayerMovement>();
+		if (other==null) return;
+		if (other.tag=="Player") Take();
 	}
 
-	void Get() {
-		Destroy(gameObject);
-	}
+	void OnTake() { }
+	void OnDrop() { }
+
+
+	public void Take() { m_TakeEvent.Invoke(); }
+	public void Drop() { m_TakeEvent.Invoke(); }
 }
+
+
+
+
+
+
+
+

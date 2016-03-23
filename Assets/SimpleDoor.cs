8ba5a739a42b34b13b3860a38c2c2ffd6e58efe3
Francisco Rojo <frojo@andrew.cmu.edu>
2016-03-22 21:24:31 -0400

    some work I guess

---
+++ Assets/SimpleDoor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleDoor : MonoBehaviour {
+
+	bool isOpen = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void ToggleOpen() {
+		if (isOpen) {
+			
+			isOpen = false;
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			return;
+	}
+}

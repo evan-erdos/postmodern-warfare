cd047bc4aa7b3f1b4c752998feb3cf3b1418c989
Francisco Rojo <frojo@andrew.cmu.edu>
2016-03-24 20:46:03 -0400

    almost all done with event

--- Assets/SpecialLevel1StoryEvent.cs
+++ Assets/SpecialLevel1StoryEvent.cs
@@ -8,13 +8,20 @@ public class SpecialLevel1StoryEvent : MonoBehaviour {
 
 	public GameObject narration;
 	public GameObject player;
+	public GameObject baddie;
 	public GameObject handgun;
+	public GameObject flyTarget;
 
 	public float gunForce = 60000f;
+	public float flySpeed = 600f;
+	public float flyDelay = 10f;
+
+	Vector3 speed;
 
 	public int currMsgIdx = 0;
 
 	public bool storyEventHappening = false;
+	public bool flyingAway = false;
 
 	public static string[] msgNames = {
 		"carl_1",
@@ -28,7 +35,28 @@ public class SpecialLevel1StoryEvent : MonoBehaviour {
 		"carl_8",
 		"carl_9",
 		"carl_10",
-		"carl_11"
+		"carl_11",
+		"narrator_6",
+		"carl_12",
+		"carl_13",
+		"carl_14",
+		"carl_15",
+		"carl_16",
+		"carl_18",
+		"carl_19",
+		"carl_20",
+		"carl_21",
+		"carl_22",
+		"carl_23",
+		"carl_24",
+		"narrator_7",
+		"narrator_8",
+		"narrator_9",
+		"narrator_10",
+		"narrator_11",
+		"narrator_12",
+		"narrator_13",
+		"narrator_14",
 	};
 
 
@@ -43,18 +71,35 @@ public class SpecialLevel1StoryEvent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (flyingAway) {
+			baddie.transform.localPosition = Vector3.SmoothDamp (
+				baddie.transform.localPosition,
+				flyTarget.transform.position, ref speed, 0.8f,
+				flyDelay, Time.deltaTime);
+			if (Vector3.SqrMagnitude(baddie.transform.localPosition - 
+				flyTarget.transform.position) < 5) 
+			//if (baddie.transform.localPosition == flyTarget.transform.position)
+				flyingAway = false;
+			return;
+		}
+
+	
+		
 		if (!storyEventHappening)
 			return;
 		if (Input.GetButtonDown ("Throw")) {
-			if (currMsgIdx == 6) {
-				GameObject gun = (GameObject) Instantiate (handgun,
-					player.transform.position, Quaternion.identity);
+			if (currMsgIdx == 4) {
+				GameObject gun = (GameObject)Instantiate (handgun,
+					                 player.transform.position, Quaternion.identity);
 				gun.transform.GetComponent<Rigidbody2D> ().AddForce (transform.right * gunForce);
+			} else if (currMsgIdx == 25) {
+				flyingAway = true;
 			} else {
 				narration.GetComponent<Narration> ().DisplayMessage (msgNames [currMsgIdx]);
 			}
 			currMsgIdx++;
 		}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D c) {
